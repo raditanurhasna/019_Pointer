0957379cdacdcf35d87da33b392ee7637747b7e5
raditanurhasna <raditanurhasna1410@gmail.com>
2023-05-10 15:14:11 +0700

    Membuat member akses

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -16,6 +16,10 @@ int main()
 	mahasiswa mhs{ 1 };		//object mhs
 	mhs.showNim();			//Member Acces Operator
 
+	mahasiswa& ref = mhs;	//Pointer Reference refMhs
+	ref.nim = 2;			//Member Acces Operator 
+	mhs.showNim();
+
 	
 
 	
